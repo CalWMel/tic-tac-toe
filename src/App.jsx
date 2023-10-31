cd9fa01d70f1777b398f728a0ac645e3ec397508
Calum Woods <2602747w@student.gla.ac.uk>
2023-10-31 21:49:09 +0000

    Added in time travel (the ability to step back to see previous moves)

--- src/App.jsx
+++ src/App.jsx
@@ -54,17 +54,21 @@ function Board({ xisNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xisNext, setXisExt] = useState(true);
+  const [xisNext, setXisNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXisExt(!xisNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXisNext(!xisNext);
   }
 
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove);
+    setXisNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
